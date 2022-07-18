1859df061c179f0a5bc1e2f30a85fd198a097549
MohamedAbouElMaali <maboue85@gmail.com>
2022-07-18 13:01:25 -0400

    Added Comments

--- src/MainTester.java
+++ src/MainTester.java
@@ -1,7 +1,14 @@
+//This is where the program is run
 public class MainTester {
+
+    //Main method
     public static void main(String[] args) {
+        //The N-Queen object
         NQueen test = new NQueen();
+
+        //Running the solve method of the N-Queen Object
         test.solve();
+
         System.out.println(test.chessBoard.getQueensPlaced());
     }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,19 +1,29 @@
+//Tile Class: Makes up th 2d ChessBoard Array and deals with classifying each square
 public class Tile {
+    //Instance Variables
+
+    //True if placing a queen on the Tile will not cause any problems
     public boolean isSafe = true;
+    //True if their is currently a queen on the Tile
     public boolean isOccupied = false;
 
+    //Sets the status of the "isSafe" boolean to the given parameter
     public void setTile(boolean isSafe) {
         this.isSafe = isSafe;
     }
 
+    //Returns the status of the "isSafe" boolean of the Tile
     public boolean isOccupied() {
         return isSafe;
     }
 
+    //Sets the value of the "isOccupied" boolean to true
     public void occupy() {
         isOccupied = true;
     }
 
+
+    //Sets the value of the "isOccupied" boolean to false
     public void unOccupy() {
         isOccupied = false;
     }
